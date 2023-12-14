6ab912840b42d8a2e9d338c01040df556605360e
ArthikRajBJain <arthikrajbhjain@gmail.com>
2023-12-14 02:55:39 +0100

    Initial Commit

---
+++ CODE/RoboticARMTest/RoboticARMTest.ino
@@ -0,0 +1,37 @@
+#include<Servo.h>
+Servo S[6];
+void setup() 
+{
+  Serial.begin(115200);
+  pinMode(3,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  S[0].attach(3);
+  S[1].attach(5);
+  S[2].attach(6);
+  S[3].attach(9);
+  S[4].attach(10);
+  S[5].attach(11);
+}
+
+void loop() 
+{
+   for(int i=0;i<6;i++)
+   {
+     S[i].write(0);
+   }
+   delay(1000);
+   for(int i=0;i<6;i++)
+   {
+     S[i].write(90);
+   }
+   delay(1000);
+   for(int i=0;i<6;i++)
+   {
+     S[i].write(180);
+   }
+   delay(1000);
+}
